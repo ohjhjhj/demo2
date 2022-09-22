2c94a8515e8930a2471f7554888bf335a82de2d3
ohjhjhj <ogmlwls@naver.com>
2022-09-22 11:42:11 +0900

    project start

---
+++ src/main/java/com/example/demo2/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.example.demo2.service;
+
+import java.util.List;
+import com.example.demo2.dto.MemberDTO;
+
+public interface MemberService {
+	List<MemberDTO> findMemberList();
+	MemberDTO findMemberDetail(String id);
+	void registerMember(MemberDTO memberDTO);
+	void removeMember(String id);
+}
